b462d0fc7b9ee1699081f940cac9d598eded9aaf
daleb <daleb015@gmail.com>
2021-12-21 15:35:51 -0500

    create components structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookingComponent } from './components/booking/booking.component';
+import { ExploreComponent } from './components/explore/explore.component';
+import { CancelComponent } from './components/cancel/cancel.component';
+import { HeaderComponent } from './components/header/header.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: '', component: ExploreComponent },
+  { path: 'booking', component: BookingComponent },
+  { path: 'cancel', component: CancelComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookingComponent,
+    ExploreComponent,
+    CancelComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes
+      ),
   ],
   providers: [],
   bootstrap: [AppComponent]
