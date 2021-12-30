7b967783d5ccdbcb6821517b992065bf692c8cf9
daleb <daleb015@gmail.com>
2021-12-30 07:47:28 -0500

    mocking services and using on view

--- src/app/components/explore/explore.component.ts
+++ src/app/components/explore/explore.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AppService } from 'src/app/services/app.service';
+import { GetRestaurantsResponseRest } from 'src/app/shared/models/get-restaurants-response-rest';
+import { RestaurantResponseRest } from 'src/app/shared/models/restaurant-response-rest';
 
 @Component({
   selector: 'app-explore',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExploreComponent implements OnInit {
 
-  constructor() { }
+  public restaurants: RestaurantResponseRest[];
+
+  constructor( private service: AppService) {
+    this.restaurants = [];
+  }
 
   ngOnInit(): void {
+    this.service.getAllRestaurantsMock().subscribe((result: GetRestaurantsResponseRest) => {
+      this.restaurants = result.data;
+    });
   }
 
 }
